9b2c75bd72d7eea96cdfd991cdf26df1197cd177
Jesus Lizama <816473@unizar.es>
2024-05-07 22:31:59 +0200

    a

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 const express = require('express');
 const { connectDB } = require('./db/db');
 const usersRouter = require('./routes/users');
-// const activitiesRouter = require('./routes/activities');
-// const barriosRouter = require('./routes/barrios');
-// const ocioRouter = require('./routes/ocio');
-// const trabajoRouter = require('./routes/trabajo');
-// const colegioRouter = require('./routes/colegio');
-// const treeRouter = require('./routes/tree');
-// const comentariosRouter = require('./routes/comentarios');
+const activitiesRouter = require('./routes/activities');
+const barriosRouter = require('./routes/barrios');
+const ocioRouter = require('./routes/ocio');
+const trabajoRouter = require('./routes/trabajo');
+const colegioRouter = require('./routes/colegio');
+const treeRouter = require('./routes/tree');
+const comentariosRouter = require('./routes/comentarios');
 
-// const swaggerMiddleware = require('./swagger/swagger'); // Importa el middleware de Swagger
+const swaggerMiddleware = require('./swagger/swagger'); // Importa el middleware de Swagger
 
 
 const app = express();
@@ -23,7 +23,7 @@ connectDB(); // Establecer conexión a la base de datos
 app.use(express.json()); // Configurar middleware para analizar el cuerpo de las solicitudes en formato JSON
   
 
-app.use('/', usersRouter); // Usar el enrutador de usuarios para las rutas '/users'
+// app.use('/', usersRouter); // Usar el enrutador de usuarios para las rutas '/users'
 // app.use('/', activitiesRouter); // Usar el enrutador de actividades para las rutas '/activities'
 
 
