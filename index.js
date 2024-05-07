acbedf5d2291eebbc2f24f6d898b74cfc7d57172
Jesus Lizama <816473@unizar.es>
2024-05-07 22:30:37 +0200

    s

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const { connectDB } = require('./db/db');
 const usersRouter = require('./routes/users');
-const activitiesRouter = require('./routes/activities');
+// const activitiesRouter = require('./routes/activities');
 // const barriosRouter = require('./routes/barrios');
 // const ocioRouter = require('./routes/ocio');
 // const trabajoRouter = require('./routes/trabajo');
@@ -24,7 +24,7 @@ app.use(express.json()); // Configurar middleware para analizar el cuerpo de las
   
 
 app.use('/', usersRouter); // Usar el enrutador de usuarios para las rutas '/users'
-app.use('/', activitiesRouter); // Usar el enrutador de actividades para las rutas '/activities'
+// app.use('/', activitiesRouter); // Usar el enrutador de actividades para las rutas '/activities'
 
 
 // En index.js
