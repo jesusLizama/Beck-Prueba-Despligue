33274d489e21d427e8882278a1153a361dfeca33
Jesus Lizama <816473@unizar.es>
2024-05-07 22:14:34 +0200

    d

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 
 const express = require('express');
-const { connectDB } = require('./db/db');
-const usersRouter = require('./routes/users');
-const activitiesRouter = require('./routes/activities');
-const barriosRouter = require('./routes/barrios');
-const ocioRouter = require('./routes/ocio');
-const trabajoRouter = require('./routes/trabajo');
-const colegioRouter = require('./routes/colegio');
-const treeRouter = require('./routes/tree');
-const comentariosRouter = require('./routes/comentarios');
-
-const swaggerMiddleware = require('./swagger/swagger'); // Importa el middleware de Swagger
+const { connectDB } = require('./db.js');
 
 
 const app = express();
@@ -21,26 +11,6 @@ app.listen(puerto, () => console.log('Servidor escuchando en el puerto', puerto)
 connectDB(); // Establecer conexión a la base de datos
 
 app.use(express.json()); // Configurar middleware para analizar el cuerpo de las solicitudes en formato JSON
-
-
-
-
-
-// app.use('/', usersRouter); // Usar el enrutador de usuarios para las rutas '/users'
-// app.use('/', activitiesRouter); // Usar el enrutador de actividades para las rutas '/activities'
-// app.use('/', barriosRouter); // Usar el enrutador de actividades para las rutas '/barrios'
-// app.use('/', ocioRouter); // Usar el enrutador de actividades para las rutas '/ocio'
-// app.use('/', trabajoRouter); // Usar el enrutador de actividades para las rutas '/trabajo'
-// app.use('/', colegioRouter); // Usar el enrutador de actividades para las rutas '/colegio'
-// app.use('/', treeRouter); // Usar el enrutador de actividades para las rutas '/tree'
-// app.use('/', comentariosRouter); // Usar el enrutador de actividades para las rutas '/comentarios'
-
-
-
-// LO DE SWAGGER!!
-
-// Utilizar el middleware de Swagger
-swaggerMiddleware(app);
   
 
 // En index.js
